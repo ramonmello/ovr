8203ac85b47ad60ea3c0a8ad8b1ea636c18364eb
marlucec <marlucecancio@gmail.com>
2018-10-04 09:37:50 -0300

    Alteração nos arquivos para conexão de banco padrão Moodle

--- ovr_modules-beta/databaseStarRead.php
+++ ovr_modules-beta/databaseStarRead.php
@@ -1,28 +1,20 @@
 <?php   
 /*
 	Script de criação das URLs e Labels dos vídeos obtidos pela busca no servidor BAVi.
-	@author Miguel Alvim
+	@author Marluce Vitor && Miguel Alvim
 	@version ALPHA
-	@date 14/06/2018
+	@date 04/10/2018
 */
-//Valores de acesso ao banco e cache do moodle
-	$dbType = 'mysql';
-	$dbHost = 'localhost';
-	$dbName = 'moodle';
-	$dbUser = 'root';
-	$dbPass = 'root';
-	$dbPort = '3306';
-	$dbChar = 'UTF8';
 
-	$conn = $mysqli = new mysqli($dbHost, $dbUser, $dbPass, $dbName,$dbPort);
-	if ($mysqli->connect_errno) {
-   		echo "(0)Erro ao acessar a base de dados: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-    	exit(0);
-	}
+require_once('../../config.php');
+defined('MOODLE_INTERNAL') || die();
+
+global $DB;
+	
 	$urls = json_decode($_POST['urls'],true);//Urls de cada video
 	$totUrls = count($urls);
 	
-	$query = "SELECT url,star,votos FROM mdl_estrela WHERE ";
+	$query = "SELECT e.url,e.star,e.votos FROM mdl_estrela as e WHERE ";
 	
 	for($i=0;$i<$totUrls;++$i){
 		$query .= "url = \"".$urls[$i]."\"";
@@ -30,18 +22,17 @@
 			$query .= " OR ";
 		}
 	}
-	$queryResult = $conn->query($query);
-	if ($queryResult === FALSE) {
-		echo "(1)Erro ao consultar a base de dados:\n".$conn->error;
-		exit(1);
-	}	
-	if ($queryResult->num_rows >0) {
-		$allResults = Array();
-		while($row = $queryResult->fetch_assoc()){
-			array_push($allResults,($row));
-		}
-		echo JSON_encode($allResults);
-	}else{
-		echo "{}";
-	}
+
+
+	$users = array_values($DB->get_records_sql($query, $params));
+
+	if ($$users=== FALSE) {
+	 	echo "(1)Erro ao consultar a base de dados:\n".$DB->error;
+
+	 	exit(1);
+	 }else{
+
+	 echo JSON_encode($users);
+	 }
+
 ?>
--- ovr_modules-beta/estrela.php
+++ ovr_modules-beta/estrela.php
@@ -1,47 +1,37 @@
 <?php   
 
-//require $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 require_once('../../config.php');
-//Valores de acesso ao banco e cache do moodle
-$dbType = 'mysql';
-$dbHost = 'localhost';
-$dbName = 'moodle';
-$dbUser = 'root';
-$dbPass = 'root';
-$dbPort = '3306';
-$dbChar = 'UTF8';
 
-$conn = $mysqli = new mysqli($dbHost, $dbUser, $dbPass, $dbName,$dbPort);
-if ($mysqli->connect_errno) {
-	echo "Erro ao atualizar a base de dados: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-	exit(0);
-}
+defined('MOODLE_INTERNAL') || die();
+global $DB;
+
 //Tranformando os valores passados por post(formatados em JSON) em arrays padrão PHP
 $url =$_POST['url'];//Url de cada video
 $stars = $_POST['stars'];//Stars de cada video
 
 //criar função para fazer essa consulta
 $query= "select * from mdl_estrela where url=\"".$url."\"";
-$queryResult = $conn->query($query);
+$params['url'] = $url;
+$queryResult = $DB->get_records_sql($query, $params);
 $userid = $USER->id;
 
-if($queryResult->num_rows > 0){
+if($queryResult!=null){
 	//echo "Alguém já votou nesse vídeo\n";
-	$row = $queryResult->fetch_assoc();
+	$row = array($queryResult);
 	$query2= "SELECT * from mdl_estrela_registro where estrelaid = ".$row['id']." AND userid = ".$userid;
-	$queryResult2 = $conn->query($query2);
+	$queryResult2 = $DB->get_records_sql($query2,$params);
 	if($queryResult2->num_rows === 0){
 		//echo "O usuario logado ainda não votou nesse vídeo\n";
 		$starAux = $row['star'];
 		$votosAux  = $row['votos'];
 		$starAux = ($starAux*$votosAux+$stars)/($votosAux+1);
 		$queryUpdate = "UPDATE mdl_estrela set star = ".$starAux.",votos=".($votosAux+1)." WHERE url=\"".$url."\"";
-		if ($conn->query($queryUpdate) === FALSE) {
+		if ($DB->get_records_sql($queryUpdate, $params) === FALSE) {
 			echo "(1)Erro ao atualizar a base de dados:\n".$conn->error;
 			exit(1);
 		}
 		$sql2 = "INSERT INTO mdl_estrela_registro (estrelaid, userid, voto) VALUES (".$row['id'].", ".$userid.", ".$stars." )";
-		if ($conn->query($sql2) === FALSE) {
+		if ($DB->get_records_sql($sql2, $params) === FALSE) {
 			echo "\n(3)Erro ao atualizar a base de dados:\n ".$conn->error;
 			exit(3);
 		}
@@ -50,15 +40,15 @@ if($queryResult->num_rows > 0){
 else{
 	//echo "Primeiro voto no vídeo";
 	$sql = "INSERT INTO mdl_estrela(url,star,votos) VALUES (\"".$url."\",".$stars.",".(1).")";
-	if ($conn->query($sql) === FALSE) {
+	if ($DB->get_records_sql($sql, $params) === FALSE) {
 		echo "(2)Erro ao atualizar a base de dados:\n".$conn->error;
 		exit(2);
 	}
 	$query= "select * from mdl_estrela where url=\"".$url."\"";
-	$queryResult = $conn->query($query);
+	$queryResult = $DB->get_records_sql($query, $params);
 	$row = $queryResult->fetch_assoc();
 	$sql2 = "INSERT INTO mdl_estrela_registro (estrelaid, userid, voto) VALUES (".$row['id'].", ".$userid.", ".$stars." )";
-	if ($conn->query($sql2) === FALSE) {
+	if ($DB->get_records_sql($sql2, $params) === FALSE) {
 		echo "\n(3)Erro ao atualizar a base de dados:\n ".$conn->error;
 		exit(3);
 	}
